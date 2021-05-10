9e5e7033c6318306e4ca41fceb2f5f8e3ad8d730
CHChang <holybible934@gmail.com>
2021-05-10 20:03:25 +0800

    Add MainActivityViewModel to load from MovementRepository

--- app/src/main/java/com/tonal/interview/viewmodel/MainActivityViewModel.kt
+++ app/src/main/java/com/tonal/interview/viewmodel/MainActivityViewModel.kt
@@ -1,4 +1,28 @@
 package com.tonal.interview.viewmodel
 
-class MainActivityViewModel {
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.tonal.interview.data.Movement
+import com.tonal.interview.data.MovementRepository
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+class MainActivityViewModel(
+    private val repo: MovementRepository
+): ViewModel() {
+    private var _movementList: List<Movement>? = null
+        set(value) {
+            field = value
+            value.let { movementList.postValue(it) }
+        }
+    val movementList: MutableLiveData<List<Movement>> = MutableLiveData()
+
+    fun loadMovementList() {
+        viewModelScope.launch {
+            with(Dispatchers.IO) {
+                _movementList = repo.fetchMovements()
+            }
+        }
+    }
 }
