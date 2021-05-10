54854b15abefecee33c281ad8880931120860081
CHChang <holybible934@gmail.com>
2021-05-10 20:25:54 +0800

    Add RecyclerViewAdapter in MainActivity

--- app/src/main/java/com/tonal/interview/MainActivity.kt
+++ app/src/main/java/com/tonal/interview/MainActivity.kt
@@ -4,10 +4,13 @@ import android.content.ContentValues.TAG
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.RecyclerView
+import com.tonal.interview.adapter.MainRecyclerViewAdapter
 import com.tonal.interview.viewmodel.MainActivityViewModel
 
 class MainActivity : AppCompatActivity() {
     private lateinit var mainViewModel: MainActivityViewModel
+    private lateinit var mainAdapter: MainRecyclerViewAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,5 +21,6 @@ class MainActivity : AppCompatActivity() {
         mainViewModel.movementList.observe(this, {
             Log.d(TAG, "onCreate: List Size is " + it.size)
         })
+        mainAdapter = MainRecyclerViewAdapter()
     }
 }
