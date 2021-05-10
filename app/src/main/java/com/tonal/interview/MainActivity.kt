4a0d52e5bc093cfb8ea1471ce419ddaf544c7aae
CHChang <holybible934@gmail.com>
2021-05-10 20:08:29 +0800

    Test mainViewModel to retrieve data from movementRepository

--- app/src/main/java/com/tonal/interview/MainActivity.kt
+++ app/src/main/java/com/tonal/interview/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.tonal.interview
 
+import android.content.ContentValues.TAG
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import com.tonal.interview.viewmodel.MainActivityViewModel
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var mainViewModel: MainActivityViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
+
+        mainViewModel = MainActivityViewModel(DependencyInjection.movementRepository)
+        mainViewModel.loadMovementList()
+        mainViewModel.movementList.observe(this, {
+            Log.d(TAG, "onCreate: List Size is " + it.size)
+        })
     }
 }
