72e1bf18f80954c2d74e2ed3184e04896fde2e64
CHChang <holybible934@gmail.com>
2021-05-10 20:17:39 +0800

    Construct RecyclerView Adapter

--- app/src/main/java/com/tonal/interview/adapter/MainRecyclerViewAdapter.kt
+++ app/src/main/java/com/tonal/interview/adapter/MainRecyclerViewAdapter.kt
@@ -1,4 +1,44 @@
 package com.tonal.interview.adapter
 
-class MainRecyclerViewAdapter {
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.tonal.interview.R
+import com.tonal.interview.data.Movement
+
+class MainRecyclerViewAdapter:
+    ListAdapter<Movement, MainRecyclerViewAdapter.MovementViewHolder>(MovementDiff()) {
+
+    inner class MovementViewHolder(private val view: View): RecyclerView.ViewHolder(view) {
+        fun bind(item: Movement?) {
+            val name = view.findViewById<TextView>(R.id.name)
+            val thumbnail = view.findViewById<ImageView>(R.id.thumbnail)
+            name.text = item?.name ?: "No Name"
+
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MovementViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: MovementViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+}
+
+class MovementDiff: DiffUtil.ItemCallback<Movement>() {
+    override fun areItemsTheSame(oldItem: Movement, newItem: Movement): Boolean {
+        return oldItem.id == newItem.id
+    }
+
+    override fun areContentsTheSame(oldItem: Movement, newItem: Movement): Boolean {
+        return oldItem.name == newItem.name
+    }
+
 }
